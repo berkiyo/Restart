bec5ae5b348ba13e94b7a27ecc806d223794da24
berkiyo <berk@tekbyte.net>
2022-03-24 21:36:49 +1100

    Part 1 done

--- Restart/Views/OnboardingView.swift
+++ Restart/Views/OnboardingView.swift
@@ -67,6 +67,11 @@ struct OnboardingView: View {
 						.padding(8)
 					
 					// 2. Call to action (static)
+					Text("Get Started")
+						.font(.system(.title3, design: .rounded))
+						.fontWeight(.bold)
+						.foregroundColor(.white)
+						.offset(x: 20)
 					
 					
 					// 3. Capsule (dynamic width)
@@ -79,7 +84,26 @@ struct OnboardingView: View {
 					}
 					
 					// 4. Circle (draggable)
-					
+					HStack {
+						ZStack {
+							Circle()
+								.fill(Color.red)
+							Circle()
+								.fill(.black.opacity(0.15))
+								.padding(8)
+							Image(systemName: "chevron.right.2")
+								.font(.system(size: 24, weight: .bold))
+						}
+						.foregroundColor(.white)
+					.frame(width: 80, height: 80, alignment: .center)
+					.onTapGesture {
+						isOnboardingViewActive = false
+					}
+						
+						
+						
+						Spacer() // push the button to the left
+					}
 				}
 				.frame(height: 80, alignment: .center)
 				.padding()
